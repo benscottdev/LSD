0ecfa39474b86307ab6ed925110257631ca2ea22
NIHLUS <benjamin.scott.mtb@gmail.com>
2024-01-30 08:39:27 +1100

    page transition changed to blur

--- src/Components/Transition.jsx
+++ src/Components/Transition.jsx
@@ -9,14 +9,14 @@ const Transition = (OgComponent) => {
         initial={{ scaleY: 0 }}
         animate={{ scaleY: 0 }}
         exit={{ scaleY: 1 }}
-        transition={{ duration: 0.8, ease: [0.22, 1, 0.36, 1] }}
+        transition={{ duration: 0.8, ease: "linear" }}
       />
       <motion.div
         className="slideOut"
         initial={{ scaleY: 1 }}
         animate={{ scaleY: 0 }}
         exit={{ scaleY: 0 }}
-        transition={{ duration: 0.8, ease: [0.22, 1, 0.36, 1] }}
+        transition={{ duration: 1, ease: "linear" }}
       />
     </>
   );
