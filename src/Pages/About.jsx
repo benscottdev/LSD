f7683ae46ab59cd0346f423159eeb25db3aa5375
NIHLUS <benjamin.scott.mtb@gmail.com>
2024-02-29 19:26:57 +1100

    langauge icons added and some copy changes

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -7,30 +7,48 @@ import pythonIcon from "../Assets/Python.png";
 import javascriptIcon from "../Assets/JavaScript.png";
 
 function About({ lightMode }) {
-  const iconArray = [reactIcon, htmlIcon, cssIcon, javascriptIcon, pythonIcon];
+  const iconArray = [
+    { language: "React", source: reactIcon },
+    { language: "JavaScript", source: javascriptIcon },
+    { language: "HTML", source: htmlIcon },
+    { language: "CSS", source: cssIcon },
+    { language: "Python", source: pythonIcon },
+  ];
 
   return (
     <div className={lightMode}>
       <HomeBtn />
       <div className="about">
         <p>
-          Yo! I'm a 23-year-old web enthusiast with a knack for crafting sleek
-          online experiences.
-          <br /> <br />
-          Fluent in CSS, HTML, JavaScript, and React, I specialize in turning
-          ideas into polished interfaces that not only look great but also
-          function seamlessly. When I'm not buried in lines of code, you'll find
-          me exploring the latest design trends or brainstorming over a cup of
-          coffee. Let's team up and bring your web visions to life in a way
-          that's both professional and effortlessly cool.
+          Yo! I'm Ben Scott, a 23-year-old self-taught web developer hailing
+          from Australia.
           <br />
           <br />
-          Let's make some digital magic happen!
+          Over the past couple of years, I've been diving headfirst into the
+          world of web development, mastering languages like React, JavaScript,
+          HTML, CSS, and Python. My journey into this field started with a
+          healthy dose of curiosity and a knack for tinkering with just about
+          anything. Through plenty of trial and error (and maybe a few
+          late-night debugging sessions), I've gained a solid grip on the
+          technologies that power the web. From crafting sleek layouts to adding
+          that extra touch of interactivity, I love the challenge of bringing
+          ideas to life with clean, efficient code. Whether it's optimizing
+          performance or troubleshooting tricky issues, I'm always up for a
+          coding adventure.
+          <br />
+          <br />
+          Thanks for dropping by, and I can't wait to chat about how we can
+          bring your next web project to life!
         </p>
 
-        <div className="icons">
+        <div className="languageIcons">
           {iconArray.map((icon) => (
-            <img id="languageIcons" key={icon} src={icon} alt="skill icon" />
+            <img
+              id="languageIcons"
+              key={icon.language}
+              src={icon.source}
+              alt="skill icon"
+            />
           ))}
         </div>
       </div>
--- src/Pages/Create.jsx
+++ src/Pages/Create.jsx
@@ -7,16 +7,18 @@ function Create({ lightMode }) {
       <div className="create">
         <HomeBtn />
         <p>
-          Hey there, welcome to my corner of the web! Ready to collaborate and
-          bring your ideas to life online? Count me in. Whether you've got
-          questions, bursts of creativity, or you're just eager to chat about
-          the latest web trends, I'm all ears. Let's roll up our sleeves and
-          create something awesome together!
+          Ready to collaborate and bring your ideas to life online? Count me in.
+          Whether you've got questions, bursts of creativity, or you're just
+          eager to chat about the latest web trends, I'm all ears. Let's roll up
+          our sleeves and create something awesome together!
+          <br />
+          <br />
+          Chat soon, Ben Scott.
           <br />
           <br />
           phone: 0498 095 622
           <br />
-          email:
+          email:{" "}
           <a href="mailto:benscott.dev@gmail.com">benscott.dev@gmail.com</a>
         </p>
       </div>
